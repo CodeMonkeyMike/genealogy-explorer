f5ed66261f086ced3b4ca000bfec581997be8256
CodeMonkeyMike <git@mikezipf.com>
2017-01-28 13:39:23 -0500

    Fiddling around with types

--- ancestry/Models/Types/Sex.php
+++ ancestry/Models/Types/Sex.php
@@ -4,24 +4,33 @@ namespace Ancestry\Models\Types;
 
 class Sex
 {
+    /** @var string */
     const MALE = 'male';
+
+    /** @var string */
     const FEMALE = 'female';
 
+    /** @var string[] */
     public static $maleSpelling = ['man', 'male'];
+
+    /** @var string[] */
     public static $femaleSpelling = ['woman', 'female'];
 
+    /** @var string[] */
     protected static $sexes = [self::MALE, self::FEMALE];
 
+    /** @param Sex|string $sex */
     public function __construct($sex)
     {
         $this->sex = $this->parse($sex);
     }
 
+    /** @param Sex|string $sex */
     public static function parse($sex): string
     {
         if (is_string($sex)) {
             $sex = strtolower($sex);
-            
+
             if (in_array($sex, self::$maleSpelling)) {
                 return self::MALE;
             } elseif (in_array($sex, self::$femaleSpelling)) {
@@ -29,8 +38,10 @@ class Sex
             }
         }
 
-        if (is_object($sex) && get_class($sex) === self::class) {
-            return self::parse($sex->get());
+        if (is_object($sex)) {
+            if (get_class($sex) === self::class) {
+                return self::parse($sex->get());
+            }
         }
 
         throw new \Exception('Not a valid sex');
@@ -46,6 +57,7 @@ class Sex
         return $this->get() === self::FEMALE;
     }
 
+    /** @param Sex|string $value */
     public function equals($value): bool
     {
         return $this->get() === self::parse($value);
